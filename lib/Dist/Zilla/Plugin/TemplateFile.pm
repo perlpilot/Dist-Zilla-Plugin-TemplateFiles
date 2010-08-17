27b79939f68b12b8348713592e7fb81b18d9a358
Jonathan Scott Duff <duff@pobox.com>
2010-08-17 16:22:01 -0500

    first hacking

--- lib/Dist/Zilla/Plugin/TemplateFile.pm
+++ lib/Dist/Zilla/Plugin/TemplateFile.pm
@@ -1,6 +1,31 @@
 package Dist::Zilla::Plugin::TemplateFile;
-# ABSTRACT:  write something clever here
+# ABSTRACT:  Use a file as a template when building a distribution
 
-use strict; use warnings;
+use Moose;
+with qw/ 
+    Dist::Zilla::Role::Plugin 
+    Dist::Zilla::Role::BeforeBuild 
+    Dist::Zilla::Role::TextTemplate 
+    Dist::Zilla::Role::FileInjector 
+/;
+
+has filename => (
+    is => 'rw',
+    isa => 'Str',
+    required => 1,
+);
+
+
+sub before_build {
+    my $self = shift;
+    my $template = Dist::Zilla::File::OnDisk->new( { name => $self->filename } );
+    my $content = $self->fill_in_string( $template->content, { dist => $self->zilla } );
+    $template->content($content);
+    print $content; exit;
+#    $self->add_file($template);
+}
+
+
+__PACKAGE__->meta->make_immutable;
 
 1;
